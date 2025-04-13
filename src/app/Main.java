ab57926211511ad84269756414f3b74a3a82cd74
MishaD <misha127831@gmail.com>
2025-04-13 21:22:57 +0300

    add convert fahrenheit to celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("App to convert fahrenheit to celsius.");
+        double C = 7;
+        double cel = convFahrenheitToCelsius(C);
+        System.out.println("Result " + cel + " celsius.");
+    }
+    private static double convFahrenheitToCelsius(double C){
+        return (C-32)/1.8;
     }
 }
